df09d9022d4b768130af6fe2748a9d869b6454d1
karasawa ai <jmdatmw08@gmail.com>
2019-04-21 15:38:53 +0900

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,6 +52,13 @@
               <li><a class="nav-link" href="{{ route('login') }}">{{__('Login')}}</a></li>
               {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
               @else
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+          @else
               <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
